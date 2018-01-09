3066ca92f53e666353b52c26409459f0f082b14a
Unknown <safetyoranges@gmail.com>
2018-01-09 14:53:39 +0100

    Add new UA (3 subclasses), new function, Wild Magic Surge Table
    Added a new function to the PHB script that allows easy addition of a string to one of the Note page sections. It is now used for the Wild Magic Surge Table and the Reckless Casting Tables

--- WotC material/all_WotC_published.js
+++ WotC material/all_WotC_published.js
@@ -1476,7 +1476,69 @@ AddSubClass("sorcerer", "wild magic", {
 			name : "Wild Magic Surge",
 			source : ["P", 103],
 			minlevel : 1,
-			description : "\n   " + "Wild Magic Surges happen 5% of the time that I cast a sorcerer spell (PHB 104)" + "\n   " + "This doesn't happen with cantrips and I only take this chance if the DM tells me to"
+			description : desc([
+				"Wild Magic Surges happen 5% of the time that I cast a sorcerer spell",
+				"This doesn't happen with cantrips and I only take this chance if the DM tells me to",
+				"See the \"Notes\" page for the table"
+			]),
+			wmsurgetable1 : "\u25C6 Wild Magic Surge Table (Wild Magic 1, PHB 104) [results 01-50]" + desc([
+				"d100  Effect",
+				"01-02 Roll on this table at the start of each of your turns for the next minute, ignoring this result on subsequent rolls.",
+				"03-04 For the next minute, you can see any invisible creature if you have line of sight to it.",
+				"05-06 A modron chosen and controlled by the DM appears in an unoccupied space within 5 ft of you, then disappears 1 minute later.",
+				"07-08 You cast fireball as a 3rd-level spell centered on yourself.",
+				"09-10 You cast magic missile as a 5th-level spell.",
+				"11-12 Roll a d10. Your height changes by a number of inches equal to the roll. If the roll is odd, you shrink. If the roll is even, you grow.",
+				"13-14 You cast confusion centered on yourself.",
+				"15-16 For the next minute, you regain 5 hit points at the start of each of your turns.",
+				"17-18 You grow a long beard made of feathers that remains until you sneeze, at which point the feathers explode out from your face.",
+				"19-20 You cast grease centered on yourself.",
+				"21-22 Creatures have disadvantage on saving throws against the next spell you cast in the next minute that involves a saving throw.",
+				"23-24 Your skin turns a vibrant shade of blue. A remove curse spell can end this effect.",
+				"25-26 An eye appears on your forehead for the next minute.",
+				"27-28 For the next minute, all your spells with a casting time feet of 1 action have a casting time of 1 bonus action.",
+				"29-30 You teleport up to 60 ft to an unoccupied space of your choice that you can see.",
+				"31-32 You are transported to the Astral Plane until the end of your next turn, after which time you return to the space you previously occupied or the nearest unoccupied space if that space is occupied.",
+				"33-34 Maximize the damage of the next damaging spell you cast within the next minute.",
+				"35-36 Roll a d10. Your age changes by a number of years equal to the roll. If the roll is odd, you get younger (minimum 1 year old). If the roll is even, you get older.",
+				"37-38 1d6 flumphs controlled by the DM appear in unoccupied spaces within 60 ft of you and are frightened of you. They vanish after 1 minute.",
+				"39-40 You regain 2d10 hit points.",
+				"41-42 You turn into a potted plant until the start of your next turn. While a plant, you are incapacitated and have vulnerability to all damage. If you drop to 0 hit points, your pot breaks, and your form reverts.",
+				"43-44 For the next minute, you can teleport up to 20 ft as a bonus action on each of your turns.",
+				"45-46 You cast levitate on yourself.",
+				"47-48 A unicorn controlled by the DM appears in a space within 5 ft of you, then disappears 1 minute later.",
+				"49-50 You can't speak for the next minute. Whenever you try, pink bubbles float out of your mouth."
+			]),
+			wmsurgetable2 : "\u25C6 Wild Magic Surge Table (Wild Magic 1, PHB 104) [results 51-100]" + desc([
+				"d100  Effect",
+				"51-52 A spectral shield hovers near you for the next minute, granting you a +2 bonus to AC and immunity to magic missile.",
+				"53-54 You are immune to being intoxicated by alcohol for the next 5d6 days.",
+				"55-56 Your hair falls out but grows back within 24 hours.",
+				"57-58 For the next minute, any flammable object you touch that isn't being worn or carried by another creature bursts into flame.",
+				"59-60 You regain your lowest-level expended spell slot.",
+				"61-62 For the next minute, you must shout when you speak.",
+				"63-64 You cast fog cloud centered on yourself.",
+				"65-66 Up to three creatures you choose within 30 ft of you take 4d10 lightning damage.",
+				"67-68 You are frightened by the nearest creature until the end of your next turn.",
+				"69-70 Each creature within 30 ft of you becomes invisible for the next minute. The invisibility ends on a creature when it attacks or casts a spell.",
+				"71-72 You gain resistance to all damage for the next minute.",
+				"73-74 A random creature within 60 ft of you becomes poisoned for 1d4 hours.",
+				"75-76 You glow with bright light in a 30-ft radius for the next minute. Any creature that ends its turn within 5 ft of you is blinded until the end of its next turn.",
+				"79-80 Illusory butterflies and flower petals flutter in the air within 10 ft of you for the next minute.",
+				"77-78 You cast polymorph on yourself. If you fail the saving throw, you turn into a sheep for the spell's duration.",
+				"81-82 You can take one additional action immediately.",
+				"83-84 Each creature within 30 ft of you takes 1d10 necrotic damage. You regain hit points equal to the sum of the necrotic damage dealt.",
+				"85-86 You cast mirror image.",
+				"87-88 You cast fly on a random creature within 60 ft of you.",
+				"89-90 You become invisible for the next minute. During that time, other creatures can't hear you. The invisibility ends if you attack or cast a spell.",
+				"91-92 If you die within the next minute, you immediately come back to life as if by the reincarnate spell.",
+				"93-94 Your size increases by one size category for the next minute.",
+				"95-96 You and all creatures within 30 ft of you gain vulnerability to piercing damage for the next minute.",
+				"97-98 You are surrounded by faint, ethereal music for the next minute.",
+				"99-100 You regain all expended sorcery points."
+			]),
+			eval : "try {AddToNotes(ClassSubList['sorcerer-wild magic'].features.subclassfeature1.wmsurgetable1, 'Wild Mage\\'s Wild Magic Surge table, part 1'); AddToNotes(ClassSubList['sorcerer-wild magic'].features.subclassfeature1.wmsurgetable2, 'Wild Mage\\'s Wild Magic Surge table, part 2');} catch (er) {};",
+			removeeval : "try {AddToNotes('', '', ClassSubList['sorcerer-wild magic'].features.subclassfeature1.wmsurgetable1); AddToNotes('', '', ClassSubList['sorcerer-wild magic'].features.subclassfeature1.wmsurgetable2);} catch (er) {};"
 		},
 		"subclassfeature1.1" : {
 			name : "Tides of Chaos",
@@ -4013,6 +4075,58 @@ SpellsList["wrathful smite"] = {
 	save : "Wis",
 	description : "Next melee weapon hit +1d6 Psychic dmg and save or frightened; it can take 1 a for extra save",
 	descriptionFull : "The next time you hit with a melee weapon attack during this spell's duration, your attack deals an extra 1d6 psychic damage. Additionally, if the target is a creature, it must make a Wisdom saving throw or be frightened of you until the spell ends. As an action, the creature can make a Wisdom check against your spell save DC to steel its resolve and end this spell."
+};
+
+// Add a function if not already defined
+try {
+	AddToNotes.toString();
+} catch (err) {
+	AddToNotes = function(noteStr, alertTxt, oldNoteStr) {
+		if (What("Unit System") === "metric") {
+			noteStr = ConvertToMetric(noteStr, 0.5);
+			if (oldNoteStr) oldNoteStr = ConvertToMetric(oldNoteStr, 0.5);
+		};
+		noteStr = noteStr.replace(/\n/g, "\r");
+		if (oldNoteStr) oldNoteStr = oldNoteStr.replace(/\n/g, "\r");
+		var replaceOldNote = false;
+		if (!isTemplVis("ASnotes")) {
+			var noteFld = DoTemplate("ASnotes", "Add");
+			noteFld += "Notes.Left";
+		} else {
+			var noteFld = false;
+			var noteFlds = ["Notes.Left", "Notes.Right"];
+			var notesPrefix = What("Template.extras.ASnotes").split(",");
+			for (var i = 1; i < notesPrefix.length; i++) {
+				for (var n = 0; n < noteFlds.length; n++) {
+					var aFld = notesPrefix[i] + noteFlds[n];
+					var inFld = What(aFld);
+					if (noteStr && inFld.toLowerCase().indexOf(noteStr.toLowerCase()) !== -1) {
+						return;
+					} else if (oldNoteStr && inFld.toLowerCase().indexOf(oldNoteStr.toLowerCase()) !== -1) {
+						noteFld = aFld;
+						replaceOldNote = true;
+						i = noteFlds.length;
+						break;
+					} else if (inFld === "" && !noteFld) {
+						noteFld = aFld;
+					};
+				};
+			};
+			if (!noteFld && noteStr) {
+				noteFld = DoTemplate("ASnotes", "Add");
+			} else if (!noteStr && !oldNoteStr) {
+				return;
+			};
+		};
+		ReplaceString(noteFld, noteStr, false, oldNoteStr ? oldNoteStr : "");
+		if (!replaceOldNote && noteStr && alertTxt) {
+			app.alert({
+				cTitle : alertTxt + " is added on the Notes page",
+				cMsg : "You can find the rules for " + alertTxt + " on the \"Notes\" page at page no. " + (tDoc.getField(noteFld).page + 1) + ".\n\nThese rules are simply to much for the Class Features section and do not fit with the rest that needs to go in the third page's Notes section. Thus, these rules will be put on a Notes page and will be updated there.",
+				nIcon : 3
+			});
+		};
+	};
 };
 var iFileName = "pub_20140819_HotDQ.js";
 RequiredSheetVersion(12.999);
---
+++ WotC material/ua_20180108_Three-Subclasses.js
@@ -0,0 +1,267 @@
+var iFileName = "ua_20180108_Three-Subclasses.js";
+RequiredSheetVersion(12.999);
+// This file adds the content from the Unearthed Arcana: Three Subclasses article to MPMB's Character Record Sheet
+
+// Define the source
+SourceList["UA:TS"] = {
+	name : "Unearthed Arcana: Three Subclasses",
+	abbreviation : "UA:TS",
+	group : "Unearthed Arcana",
+	url : "https://media.wizards.com/2018/dnd/downloads/UA-3Subclasses0108.pdf",
+	date : "2018/01/08"
+};
+
+// Adds 3 subclasses: 1 for the Druid, 1 for the Fighter, and 1 for the Wizard
+AddSubClass("druid", "circle of spores", {
+	regExpSearch : /^(?=.*(druid|shaman))(?=.*spores).*$/i,
+	subname : "Circle of Spores",
+	source : ["UA:TS", 1],
+	spellcastingExtra : ["gentle repose", "ray of enfeeblement", "animate dead", "gaseous form", "blight", "confusion", "cloudkill", "contagion"],
+	features : {
+		"subclassfeature2" : {
+			name : "Circle Spells",
+			source : ["UA:TS", 1],
+			minlevel : 2,
+			description : desc([
+				"My link to the cycle of life and death gives me with the ability to cast certain spells",
+				"These are always prepared, but don't count against the number of spells I can prepare",
+				"In addition, I learn the Chill Touch cantrip"
+			]),
+			spellcastingBonus : {
+				name : "Circle Spells",
+				spells : ["chill touch"],
+				selection : ["chill touch"],
+				atwill : true
+			}
+		},
+		"subclassfeature2.1" : {
+			name : "Halo of Spores",
+			source : ["UA:TS", 1],
+			minlevel : 2,
+			description : "\n   " + "As a reaction on my turn, I can do poison damage to one creature I can see within 10 ft",
+			additional : levels.map(function (n) { return n < 2 ? "" : (n < 6 ? 3 : n < 10 ? 6 : n < 14 ? 9 : 12) + " poison damage"; }),
+			action : ["reaction", " (on my turn)"]
+		},
+		"subclassfeature2.2" : {
+			name : "Symbiotic Entity",
+			source : ["UA:TS", 1],
+			minlevel : 2,
+			description : desc([
+				"As an action, I expend a Wild Shape use to boost my spores instead of transforming",
+				"I then gain 3 temporary HP per druid level and my Halo of Spores does double damage",
+				"Also, my melee weapon attacks do +1d6 poison damage with every hit",
+				"This lasts for 10 minutes or until I use Wild Shape again"
+			]),
+			additional : levels.map(function (n) {
+				return n < 2 ? "" : Math.floor(n*3) + " temp HP; Halo of Spores: " + (n < 6 ? 6 : n < 10 ? 12 : n < 14 ? 18 : 24) + " damage";
+			}),
+			action : ["action", ""],
+			calcChanges : {
+				atkAdd : ["if (isMeleeWeapon && (/\\b(spore|symbiotic)\\b/i).test(WeaponText) && !isNaturalWeapon) {fields.Description += (fields.Description ? '; ' : '') + '+1d6 poison damage';}; ", "If I include the word 'Spore' or 'Symbiotic' in a melee weapon's name or description, it gets treated as a weapon that is infused by my Symbiotic Entity feature, adding +1d6 poison damage in the description."]
+			}
+		},
+		"subclassfeature6" : {
+			name : "Fungal Infestation",
+			source : ["UA:TS", 2],
+			minlevel : 6,
+			description : desc([
+				"If I kill a humanoid with my Halo of Spores, it rises as a zombie at the end of my turn",
+				"This zombie has 1 HP, only takes the Attack action, and can only make 1 attack with it",
+				"It follows my mental commands and takes it turn after mine; It lasts for 1 hour"
+			])
+		},
+		"subclassfeature10" : {
+			name : "Spreading Spores",
+			source : ["UA:TS", 2],
+			minlevel : 10,
+			description : desc([
+				"As a bonus action, I create a 10-ft cube of fungal spores within 30 ft, lasting for 1 min",
+				"Any creature starting its turn in the cube takes the damage of my Halo of Spores",
+				"The cube ends if I use this again; While the cube persists, I can't use my Halo of Spores"
+			]),
+			action : ["bonus action", ""]
+		},
+		"subclassfeature14" : {
+			name : "Fungal Body",
+			source : ["UA:TS", 2],
+			minlevel : 14,
+			description : "\n   " + "I'm immune to critical hits and to being blinded, deafened, frightened, and poisoned",
+			savetxt : { immune : ["blinded", "deafened", "frightened", "poisoned", "critical hits"] }
+		}
+	}
+});
+AddSubClass("fighter", "brute", {
+	regExpSearch : /brute/i,
+	subname : "Brute",
+	source : ["UA:TS", 2],
+	fullname : "Brute",
+	features : {
+		"subclassfeature3" : {
+			name : "Brute Force",
+			source : ["UA:TS", 2],
+			minlevel : 3,
+			description : "\n   " + "I do additional damage with weapons that I'm proficient with",
+			additional : levels.map(function (n) { 
+				return n < 3 ? "" : "+1d" + (n < 10 ? 4 : n < 16 ? 6 : n < 20 ? 8 : 10) + " weapon damage";
+			}),
+			calcChanges : {
+				atkAdd : ["if (classes.known.fighter && classes.known.fighter.level > 2 && !isSpell && !isNaturalWeapon && fields.Proficiency) {fields.Description += (fields.Description ? '; ' : '') + '+1d' + (classes.known.fighter.level < 10 ? 4 : classes.known.fighter.level < 16 ? 6 : classes.known.fighter.level < 20 ? 8 : 10) + ' damage'}; ", "I do +1d4 damage with weapons that I'm proficient with. This increases to 1d6 at 10th level, 1d8 at 16th level, and 1d10 at 20th level."]
+			}
+		},
+		"subclassfeature7" : {
+			name : "Brutish Durability",
+			source : ["UA:TS", 2],
+			minlevel : 7,
+			description : desc([
+				"I add +1d6 to all my saving throws, including death saves",
+				"If the total of a death save is 20 or more, it counts as rolling a 20"
+			]),
+			savetxt : { text : ["Add 1d6 to all saves"] }
+		},
+		"subclassfeature10" : function () {
+			var FSfea = newObj(Base_ClassList.fighter.features["fighting style"]);
+			FSfea.name = "Additional Fighting Style";
+			FSfea.source = ["UA:TS", 2];
+			FSfea.minlevel = 10;
+			FSfea.description = "\n   " + "Choose an Additional Fighting Style using the \"Choose Feature\" button above ";
+			return FSfea;
+		}(),
+		"subclassfeature15" : {
+			name : "Devastating Critical",
+			source : ["UA:TS", 2],
+			minlevel : 15,
+			description : "\n   " + "Whenever I score a critical hit with a weapon, I add my fighter level to the damage",
+			additional : levels.map(function (n) { return n < 15 ? "" : "+" + n + " damage on crit"; })
+		},
+		"subclassfeature18" : {
+			name : "Survivor",
+			source : ["UA:TS", 2],
+			minlevel : 18,
+			description : desc([
+				"If I have less than half my max HP at the start of my turn, I heal myself",
+				"I regain HP equal to 5 + Constitution modifier (min 1); This doesn't work if I'm at 0 HP"
+			])
+		}
+	}
+});
+AddSubClass("wizard", "school of invention", {
+	regExpSearch : /^(?=.*wizard)(?=.*invent(ion|or)).*$/i,
+	subname : "School of Invention",
+	source : ["UA:TS", 2],
+	fullname : "Wizard (Inventor)",
+	features : {
+		"subclassfeature2" : {
+			name : "Tools of the Inventor",
+			source : ["UA:TS", 3],
+			minlevel : 2,
+			description : "\n   " + "I gain proficiency with light armor and two tools of my choice",
+			toolProfs : [["Any one tool", 2]],
+			armor : [true, false, false, false]
+		},
+		"subclassfeature2.1" : {
+			name : "Arcanomechanical Armor",
+			source : ["UA:TS", 3],
+			minlevel : 2,
+			description : desc([
+				"After a long rest, I can transform a studded leather armor into arcanomechanical armor",
+				"This is a magic light armor that gives AC 12 + Dex mod and force resistance, if attuned",
+				"Only I can attune to it; Creating a new one removes the magic from the previous"
+			]),
+			dmgres : ["Force"],
+			addarmor : "Arcanomechanical"
+		},
+		"subclassfeature2.2" : {
+			name : "Reckless Casting",
+			source : ["UA:TS", 3],
+			minlevel : 2,
+			description : desc([
+				"As an action, I can attempt to cast a random spell or cantrip by rolling a d10 on a table",
+				"On a roll of 10, I roll again twice on that table and cast both results",
+				"However, if I then roll 10 on either dice, the whole casting fails and the action is wasted",
+				"If I choose cantrip, I roll on the table once and cast the result",
+				"If I choose a spell, I expend a spell slot and roll twice on the table for the slot's level",
+				"I can then choose which of the results I use; Use 5th-level table for spell slots over level 5"
+			]),
+			castingTable : "\u25C6 Reckless Casting Tables (School of Invention 2, UA:TS 3)\nd10\tCantrip\t\td10\tCantrip" + desc([
+				" 1\tAcid Splash\t\t    6\tRay of Frost",
+				" 2\tChill Touch\t\t    7\tShocking Grasp",
+				" 3\tFire Bolt\t\t    8\tSacred Flame",
+				" 4\tLight\t\t    9\tThorn Whip",
+				" 5\tPoison Spray\t  10\tRoll twice; Another 10, all is wasted"
+			]) + "\n\nd10\t1st-Level Spell\td10\t1st-Level Spell" + desc([
+				" 1\tBurning Hands\t    6\tFog Cloud",
+				" 2\tChromatic Orb\t    7\tJump",
+				" 3\tColor Spray\t\t    8\tMagic Missile",
+				" 4\tFaerie Fire\t\t    9\tThunderwave",
+				" 5\tFalse Life\t\t  10\tRoll twice; Another 10, all is wasted"
+			]) + "\n\nd10\t2nd-Level Spell\td10\t2nd-Level Spell" + desc([
+				" 1\tBlur\t\t    6\tLevitate",
+				" 2\tDarkness\t\t    7\tMelf's Acid Arrow",
+				" 3\tEnlarge/Reduce\t    8\tScorching Ray",
+				" 4\tGust of Wind\t    9\tShatter",
+				" 5\tInvisibility\t\t  10\tRoll twice; Another 10, all is wasted"
+			]) + "\n\nd10\t3rd-Level Spell\td10\t3rd-Level Spell" + desc([
+				" 1\tBlink\t\t    6\tGaseous Form",
+				" 2\tFear\t\t    7\tLightning Bolt",
+				" 3\tFeign Death \t    8\tSleet Storm",
+				" 4\tFireball\t\t    9\tStinking Cloud",
+				" 5\tFly\t\t  10\tRoll twice; Another 10, all is wasted"
+			]) + "\n\nd10\t4th-Level Spell\td10\t4th-Level Spell" + desc([
+				" 1\tBlight\t\t    6\tIce Storm",
+				" 2\tConfusion\t\t    7\tPhantasmal Killer",
+				" 3\tEvard's Black Tentacles\t    8\tStoneskin",
+				" 4\tFire Shield\t\t    9\tWall of Fire",
+				" 5\tGreater Invisibility\t  10\tRoll twice; Another 10, all is wasted"
+			]) + "\n\nd10\t5th-Level Spell\td10\t5th-Level Spell" + desc([
+				" 1\tCloudkill\t\t    6\tInsect Plague",
+				" 2\tCone of Cold\t    7\tMass Cure Wounds",
+				" 3\tDestructive Wave\t    8\tWall of Force",
+				" 4\tFlame Strike\t    9\tWall of Stone",
+				" 5\tHold Monster\t  10\tRoll twice; Another 10, all is wasted"
+			]),
+			eval : "try {AddToNotes(ClassSubList['wizard-school of invention'].features['subclassfeature2.2'].castingTable, 'School of Invention\\'s Reckless Casting tables');} catch (er) {};",
+			removeeval : "try {AddToNotes('', '', ClassSubList['wizard-school of invention'].features['subclassfeature2.2'].castingTable);} catch (er) {};"
+		},
+		"subclassfeature6" : {
+			name : "Alchemical Casting",
+			source : ["UA:TS", 4],
+			minlevel : 6,
+			description : desc([
+				"I can manipulate spells cast while wearing and attuned to my Arcanomechanical Armor",
+				"By expending an additional 1st or 2nd-level spell slot, I do the following:",
+				"\u2022 1st: I can substitute acid, cold, fire, lightning, or thunder damage for another type",
+				"\u2022 2nd: I increase the spells damage, if any, by 2d10 force damage against one target"
+			])
+		},
+		"subclassfeature10" : {
+			name : "Prodigious Inspiration",
+			source : ["UA:TS", 4],
+			minlevel : 10,
+			description : "\n   " + "As a bonus action, I can replace a prepared spell with another from my spellbook",
+			usages : 1,
+			recovery : "short rest",
+			action : ["bonus action", ""]
+		},
+		"subclassfeature14" : {
+			name : "Controlled Chaos",
+			source : ["UA:TS", 4],
+			minlevel : 14,
+			description : desc([
+				"When I expend a spell slot to cast a spell using Reckless Casting, it is more powerful",
+				"I can roll on the table that is one level higher than the spell slot expended"
+			])
+		}
+	}
+});
+// School of Invention's Arcanomechanical Armor
+ArmourList["arcanomechanical"] = {
+	regExpSearch : /arcanomechanical/i,
+	name : "Arcanomechanical",
+	source : ["UA:TS", 3],
+	type : "light",
+	ac : 12,
+	stealthdis : false,
+	weight : 8,
+	strReq : 0,
+	invName : "Arcanomechanical armor"
+};
