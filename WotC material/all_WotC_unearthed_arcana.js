781811aace4d4c874660be43c5c7b94f9c19389a
Unknown <safetyoranges@gmail.com>
2018-06-12 15:59:04 +0200

    Fix for the Aasimar's height/weight and its Revenant
    The Revenant Aasimar from UA: Gothic Heroes was missing the Light Bringer feature. Thanks /u/RebelMage for pointing this out!

--- WotC material/all_WotC_unearthed_arcana.js
+++ WotC material/all_WotC_unearthed_arcana.js
@@ -2170,7 +2170,7 @@ SourceList["UA:GH"] = {
 };
 
 // Adds 8 new races, the Revenant versions of the Aasimar, Dragonborn, Dwarf, Elf, Gnome, Halfling, Human, and Tiefling
-RaceList["aasimar revenant"] = { //based on the VGtM Aasimar
+RaceList["aasimar revenant"] = { // Based on the VGtM Aasimar, made with /u/RebelMage's help
 	regExpSearch : /^(?=.*revenant)((?=.*aasimar)|((?=.*planetouched)(?=.*(celestial|angel)))).*$/i,
 	name : "Aasimar Revenant",
 	sortname : "Revenant, Aasimar",
@@ -2184,13 +2184,20 @@ RaceList["aasimar revenant"] = { //based on the VGtM Aasimar
 	vision : [["Darkvision", 60]],
 	dmgres : ["Necrotic", "Radiant"],
 	age : " reach adulthood in their late teens and live around 160 years",
-	height : " range from 5 to over 6 feet tall (4'9\" + 2d8\")",
-	weight : " weigh around 155 lb (110 + 2d8 \xD7 2d4 lb)",
-	heightMetric : " range from 1,5 to over 1,8 metres tall (145 + 5d8 cm)",
-	weightMetric : " weigh around 70 kg (50 + 5d8 \xD7 4d4 / 10 kg)",
+	height : " range from barely 5 to well over 6 feet tall (4'8\" + 2d10\")",
+	weight : " weigh around 165 lb (110 + 2d10 \xD7 2d4 lb)",
+	heightMetric : " range from barely 1,5 to well over 1,8 metres tall (145 + 5d10 cm)",
+	weightMetric : " weigh around 75 kg (50 + 5d10 \xD7 4d4 / 10 kg)",
 	improvements : "Aasimar Revenant: +1 Constitution, +2 Charisma;",
 	scores : [1, 0, 0, 0, 0, 2],
-	trait : "Aasimar Revenant (+1 Constitution, +2 Charisma)" + (typePF ? "\n" : " ") + "Healing Hands: As an action, once per long rest, I can touch to heal for my level in HP.\nRelentless Nature: I have returned to life with one goal: avenge my death or finish an unresolved task. I will rest once I fulfill my goal, but until then I can't truly die. Whenever I'm below half my max HP at the start of my turn, I regain 1 HP. If I die, I return to life within 24 hours. If my body was destroyed, it is reformed within 1 mile of where I died. I always know the distance and direction to creatures involved with my goal.",
+	trait : "Aasimar Revenant (+1 Constitution, +2 Charisma)" + (typePF ? "\n" : " ") + "Light Bearer: I know the Light cantrip. Healing Hands: As an action, once per long rest, I can touch to heal for my level in HP.\nRelentless Nature: I have returned to life with one goal: avenge my death or finish an unresolved task. I will rest once I fulfill my goal, but until then I can't truly die. Whenever I'm below half my max HP at the start of my turn, I regain 1 HP. If I die, I return to life within 24 hours. If my body was destroyed, it is reformed within 1 mile of where I died. I always know the distance and direction to creatures involved with my goal.",
+	spellcastingAbility : 6,
+	spellcastingBonus : {
+		name : "Light Bearer",
+		spells : ["light"],
+		selection : ["light"],
+		atwill : true
+	},
 	features : {
 		"healing hands" : {
 			name : "Healing Hands",
