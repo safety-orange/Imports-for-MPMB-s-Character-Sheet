4cc453b37bc948eedfdf144997e50519417986f4
Ben Williams <ben@smash.mn>
2020-02-28 10:30:45 +0000

    Use gulp for platform agnostic minification and concatenation This also removes superfluous `iFileName`s and `RequiredSheetVersion`s

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 This git repository holds different fan-created materials that can be used with the popular MorePurpleMoreBetter's Character Record Sheet. The repository for the sheet is [found here](https://github.com/morepurplemorebetter/MPMBs-Character-Record-Sheet).
 
 Discussion about this repository is found at https://www.reddit.com/r/mpmb/
+
+# Minification
+
+## Setup
+
+Ensure you have `node` and `npm` installed then:
+```
+npm install
+```
+
+## Use
+To minify all version (stable and beta) run:
+```
+npm run minify
+```
+Just stable:
+```
+npm run minifyStable
+```
+Just beta:
+```
+npm run minifyBeta
+```
---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+/*jshint esversion: 6 */
+
+const { series, parallel, src, dest } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const concat = require('gulp-concat');
+const del = require('gulp-rm-lines');
+const replace = require('gulp-replace');
+const log = require('fancy-log');
+
+const stableVersion = 12.999;
+const betaVersion = 13;
+const betaFolder = "/v13";
+
+function concatAndMin(glob, fileName, beta) {
+	log.info(`Minifying and concatenating type '${glob}' for ${beta ? `beta (${betaVersion})` : `stable (${stableVersion})`} version`);
+	const folder = `WotC material${beta ? betaFolder : ''}`;
+	const requiredVersion = beta ? betaVersion : stableVersion;
+	return src([`${folder}/${glob}_*.js`, `!${folder}/${glob}_*_dupl.js`])
+		.pipe(del({
+			filters: [
+				/RequiredSheetVersion\([0-9.]+\);/
+			]
+		}))
+		.pipe(replace(/var iFileName ?= ?['"]([^"']+)['"];/,"// $1"))
+		.pipe(concat(`${fileName}.js`, {newLine: '\n'}))
+		.pipe(header(`var iFileName = "${fileName}.js";\nRequiredSheetVersion(${requiredVersion});\n`))
+		.pipe(dest(folder))
+		.pipe(uglify())
+		.pipe(replace(`${fileName}.js`, `${fileName}.min.js`))
+		.pipe(rename({extname: '.min.js'}))
+		.pipe(dest(folder));
+}
+
+function combine(minified, beta) {
+	const folder = `WotC material${beta ? betaFolder : ''}`;
+	const fileHead = 'all_WotC_';
+	const path = `${folder}/${fileHead}`;
+	const ext = minified ? '.min.js' : '.js';
+	const fileName = `${fileHead}pub+UA${ext}`;
+	const requiredVersion = beta ? betaVersion : stableVersion;
+	return src([`${path}published${ext}`, `${path}unearthed_arcana${ext}`])
+		.pipe(replace(/var iFileName ?= ?['"]([^"']+)['"];\n?/, ""))
+		.pipe(replace(/RequiredSheetVersion\([0-9.]+\)[,;]\n?/, ""))
+		.pipe(concat(fileName, {newLine: minified ? '' : '\n'}))
+		.pipe(header(`var iFileName = "${fileName}";${minified ? '' : '\n'}RequiredSheetVersion(${requiredVersion})${minified ? ',' : ';\n'}`))
+		.pipe(dest(folder));
+}
+
+const combineUnminifiedBuilder = (beta) => {
+	const combineUnminified = () => {
+		log.info(`Combining Unminified Published and UA for ${beta ? `beta (${betaVersion})` : `stable (${stableVersion})`} version`);
+		return combine(false, beta);
+	}
+	return combineUnminified;
+};
+const combineMinifiedBuilder = (beta) => {
+	const combineMinified = () => {
+		log.info(`Combining Minified Published and UA for ${beta ? `beta (${betaVersion})` : `stable (${stableVersion})`} version`);
+		return combine(true, beta);
+	}
+	return combineMinified;
+};
+
+const combineAllBuilder = (beta) => {
+	const combineAll = parallel(combineUnminifiedBuilder(beta), combineMinifiedBuilder(beta));
+	return combineAll;
+};
+
+function concatAndMinPub() {
+	return concatAndMin("pub", "all_WotC_published");
+}
+
+function concatAndMinUA() {
+	return concatAndMin("ua", "all_WotC_unearthed_arcana");
+}
+
+const combineStable = combineAllBuilder();
+
+function concatAndMinPubBeta() {
+	return concatAndMin("pub", "all_WotC_published", true);
+}
+
+function concatAndMinUABeta() {
+	return concatAndMin("ua", "all_WotC_unearthed_arcana", true);
+}
+
+const combineBeta = combineAllBuilder(true);
+
+const minifyStable = series(
+	parallel(
+		concatAndMinPub,
+		concatAndMinUA
+	),
+	combineStable
+);
+
+const minifyBeta = series(
+	parallel(
+		concatAndMinPubBeta,
+		concatAndMinUABeta
+	),
+	combineBeta
+);
+
+const minify = parallel(
+	minifyStable,
+	minifyBeta
+);
+
+exports.minifyStable = minifyStable;
+exports.minifyBeta = minifyBeta;
+exports.minify = minify;
+exports.default = minify;
