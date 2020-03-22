37d1057c534ebbeaddc560e69a7acafad069dd2e
Ben Williams <ben@smash.mn>
2020-03-22 01:40:57 +0000

    Initial Explorer's Guide to Wildemount commit Adding automation (but no descriptions) for the 3 subclasses

---
+++ WotC material/v13/pub_20200317_EGtW.js
@@ -0,0 +1,284 @@
+var iFileName = "pub_20200317_EGtW.js";
+RequiredSheetVersion(13);
+// This file adds the backgrounds and beasts from Xanathar's Guide to Everything to MPMB's Character Record Sheet
+
+// Define the source
+SourceList.W={
+	name : "Explorer's Guide to Wildemount",
+	abbreviation : "EGtW",
+	group : "Primary Sources",
+	url : "https://dnd.wizards.com/products/wildemount",
+	date : "2020/03/17"
+};
+
+// Add one subclass for Fighter
+AddSubClass("fighter", "echo knight-egtw", {
+	regExpSearch : /^(?=.*echo)(?=.*knight).*$/i,
+	subname : "Echo Knight",
+	source : ["W", 183],
+	fullname : "Echo Knight",
+	features : {
+		"subclassfeature3" : {
+			name : "Manifest Echo",
+			source : ["W", 183],
+			minlevel : 3,
+			description : desc([
+			]),
+			action: [["bonus action", " (summon)"], ["bonus action", " (swap)"]],
+			eval : function (lvl, chc) {
+				companionUtil.add("Echo");
+			},
+			removeeval : function (lvl, chc) {
+				companionUtil.remove("echo");
+				if (CreatureList.echo && CreatureList.echo.removeeval) CreatureList.echo.removeeval();
+			}
+		},
+		"subclassfeature3.1" : {
+			name : "Unleash Incarnation",
+			source : ["W", 183],
+			minlevel : 3,
+			description : desc([
+			]),
+			usages : "Constitution modifier per ",
+			usagescalc : usagescalcStr("Con"),
+			recovery : "long rest"
+		},
+		"subclassfeature7" : {
+			name : "Echo Avatar",
+			source : ["W", 183],
+			minlevel : 7,
+			description : desc([
+			]),
+			action : [["action", ""]]
+		},
+		"subclassfeature10" : {
+			name : "Shadow Martyr",
+			source : ["W", 183],
+			minlevel : 10,
+			description : desc([
+			]),
+			action : [["reaction", ""]],
+			usages : 1,
+			recovery : "short rest"
+		},
+		"subclassfeature15" : {
+			name : "Reclaim Potential",
+			source : ["W", 184],
+			minlevel : 15,
+			description : desc([
+			]),
+			usages : "Constitution modifier per ",
+			usagescalc : usagescalcStr("Con"),
+			recovery : "long rest"
+		},
+		"subclassfeature18" : {
+			name : "Legion of One",
+			source : ["W", 184],
+			minlevel : 18,
+			description : desc([
+			])
+		}
+	}
+});
+
+CreatureList.echo = {
+	name : "Echo",
+	source : [["W", 183]],
+	size : 3,
+	type : "",
+	subtype : "",
+	alignment : "Neutral",
+	ac : 14,
+	hp : 1,
+	hd : [],
+	speed : "0 ft",
+	scores : [10, 10, 10, 10, 10, 10],
+	saves : ["", "", "", "", "", ""],
+	condition_immunities : "all conditions",
+	passivePerception : 0,
+	senses : "",
+	languages : "",
+	challengeRating : "0",
+	proficiencyBonus : 0,
+	attacksAction : 0,
+	attacks : [],
+	eval : function(prefix) {
+
+		// HP is only ever 1
+		var HPmaxFld = tDoc.getField(prefix + "Comp.Use.HP.Max");
+		HPmaxFld.readonly = true;
+		Hide(prefix + "Buttons.Comp.Use.HP.Max");
+
+		// Echo type
+		var theType = tDoc.getField(prefix + 'Comp.Type');
+		theType.readonly = true;
+		theType.value = 'Echo';
+
+		// Armour class is 14 + character proficiency
+		var armourClass = tDoc.getField(prefix + 'Comp.Use.AC');
+		armourClass.readonly = true;
+		armourClass.setAction("Calculate", "event.value = 14 + Number(How('Proficiency Bonus'));");
+
+		// Same size as character
+		PickDropdown(prefix + "Comp.Desc.Size", CurrentRace.size);
+
+		// Saving throws are the same as the character's
+		for (var i = 0; i < AbilityScores.abbreviations.length; i++) {
+			var abi = AbilityScores.abbreviations[i];
+			var saveModBonus = tDoc.getField(prefix + 'BlueText.Comp.Use.Ability.' + abi + '.ST.Bonus');
+			saveModBonus.readonly = true;
+			saveModBonus.setAction("Calculate", "event.value = What('" + abi + " ST Mod');");
+		}
+		// TODO: This doesn't quite work, as this bonus is unfortunately calculated after the actual save.
+	}
+};
+
+// Add two subclasses for Wizard
+AddSubClass("wizard", "chronurgy magic-egtw", {
+	regExpSearch : /^((?=.*chronurgy)(?=.*(wizard|magic|mage))|chronomancer|chronurgist).*$/i,
+	subname : "Chronurgy Magic",
+	source : ["W", 184],
+	fullname : "Chronurgist",
+	features : {
+		"subclassfeature2" : {
+			name : "Chronal Shift",
+			source : ["W", 184],
+			minlevel : 2,
+			description : desc([
+			]),
+			action : [["reaction", ""]],
+			usages : 2,
+			recovery : "long rest"
+		},
+		"subclassfeature2.1" : {
+			name : "Temporal Awareness",
+			source : ["W", 184],
+			minlevel : 2,
+			description : "\n   " + "I gain a bonus to my initiative rolls equal to my Intelligence modifier",
+			addMod : { type : "skill", field : "Init", mod : "max(Int|0)", text : "I can add my Intelligence modifier to initiative rolls." }
+		},
+		"subclassfeature6" : {
+			name : "Momentary Stasis",
+			source : ["W", 184],
+			minlevel : 6,
+			description : desc([
+			]),
+			action : [["action", ""]],
+			usages : "Intelligence modifier per ",
+			usagescalc : usagescalcStr("Int"),
+			recovery : "long rest"
+		},
+		"subclassfeature10" : {
+			name : "Arcane Abeyance",
+			source : ["W", 184],
+			minlevel : 10,
+			description : desc([
+			]),
+			usages: 1,
+			recovery: "short rest"
+		},
+		"subclassfeature14" : {
+			name : "Convergent Future",
+			source : ["W", 185],
+			minlevel : 14,
+			description : desc([
+			]),
+			action : [["reaction", ""]]
+		}
+	}
+});
+
+AddSubClass("wizard", "graviturgy magic-egtw", {
+	regExpSearch : /^((?=.*graviturgy)(?=.*(wizard|magic|mage))|gravimancer|graviturgist).*$/i,
+	subname : "Graviturgy Magic",
+	source : ["W", 185],
+	fullname : "Graviturgist",
+	features : {
+		"subclassfeature2" : {
+			name : "Adjust Density",
+			source : ["W", 185],
+			minlevel : 2,
+			description : desc([
+			]),
+			action : [["action", ""]]
+		},
+		"subclassfeature6" : {
+			name : "Gravity Well",
+			source : ["W", 185],
+			minlevel : 6,
+			description : desc([
+			])
+		},
+		"subclassfeature10" : {
+			name : "Violent Attraction",
+			source : ["W", 185],
+			minlevel : 10,
+			description : desc([
+			]),
+			action : [["reaction", ""]],
+			usages : "Intelligence modifier per ",
+			usagescalc : usagescalcStr("Int"),
+			recovery : "long rest"
+		},
+		"subclassfeature14" : {
+			name : "Event Horizon",
+			source : ["W", 185],
+			minlevel : 14,
+			description : desc([
+			]),
+			action : [["action", ""]],
+			usages : 1,
+			recovery: "long rest",
+			altResource : "SS 3+",
+		}
+	}
+});
+
+function usagescalcStr(mod) {
+	return "event.value = Math.max(1, What('" + mod + " Mod'));";
+}
+
+var companionUtil = {
+	add : function (compName) {
+		var AScompA = isTemplVis('AScomp') ? What('Template.extras.AScomp').split(',') : false;
+		var prefix = false;
+		if (AScompA) {
+			for (var a = 1; a < AScompA.length; a++) {
+				if (!What(AScompA[a] + 'Comp.Race')) {
+					prefix = AScompA[a];
+					break;
+				}
+			}
+		}
+		if (!prefix) prefix = DoTemplate('AScomp', 'Add');
+		Value(prefix + 'Comp.Race', compName);
+		var changeMsg = "The " + compName + " has been added to the companion page at page number " + (tDoc.getField(prefix + 'Comp.Race').page + 1);
+		CurrentUpdates.types.push("notes");
+		if (!CurrentUpdates.notesChanges) {
+			CurrentUpdates.notesChanges = [changeMsg];
+		} else {
+			CurrentUpdates.notesChanges.push(changeMsg);
+		}
+		return prefix;
+	},
+	remove : function (compName) {
+		var AScompA = isTemplVis('AScomp') ? What('Template.extras.AScomp').split(',') : false;
+		if (!AScompA) return;
+		compName = compName.toLowerCase();
+		for (var a = 1; a < AScompA.length; a++) {
+			if (What(AScompA[a] + 'Comp.Race').toLowerCase().indexOf(compName) !== -1) {
+				DoTemplate("AScomp", "Remove", AScompA[a], true);
+			}
+		}
+	},
+	find : function (compName) {
+		var AScompA = isTemplVis('AScomp') ? What('Template.extras.AScomp').split(',') : false;
+		var prefixes = [];
+		if (!AScompA) return prefixes;
+		compName = compName.toLowerCase();
+		for (var a = 1; a < AScompA.length; a++) {
+			if (What(AScompA[a] + 'Comp.Race').toLowerCase().indexOf(compName) !== -1) prefixes.push(AScompA[a]);
+		}
+		return prefixes;
+	}
+};
