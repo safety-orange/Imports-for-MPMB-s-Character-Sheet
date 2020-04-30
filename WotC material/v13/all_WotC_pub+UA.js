1f8d7937ed6efd70db230aa0e5a6fcfe69be1acc
BraabHimself <63005585+BraabHimself@users.noreply.github.com>
2020-04-30 00:07:22 -0400

    Oversized Longbow v13
    Add Oversized Longbow and Oversized Arrows for v13

--- WotC material/v13/all_WotC_pub+UA.js
+++ WotC material/v13/all_WotC_pub+UA.js
@@ -20596,6 +20596,30 @@ SourceList["WDH"] = {
 	date : "2018/09/18"
 };
 
+// Mundane Items
+
+WeaponsList["oversized longbow"] = {
+	regExpSearch : /^(?=.*oversized?)(?=.*(long)?bow).*$/i,
+	name : "Oversized Longbow",
+	source : ["WDH", 201],
+	list : "ranged",
+	ability : 1,
+	type : "Martial",
+	damage : [2, 6, "piercing"],
+	range : "150/600 ft",
+	weight : 2,
+	description : "Ammunition, heavy, two-handed; Can only be wielded by a Medium creature w/ 18 or more Strength",
+	abilitytodamage : true,
+	ammo : "oversized arrow"
+};
+
+AmmoList["oversized arrow"] = {
+	name : "Oversized Arrows",
+	source : ["WDH", 201],
+	icon : "Arrows",
+	weight : .10
+};
+
 // Magic Items
 var azuredgeFullDescription = "Forged by the archwizard Ahghairon, this intelligent battleaxe was crafted to defend Waterdeep. Its current wielder is a former member of Force Grey named Meloon Wardragon, but the weapon is searching for a new owner.\n   Azuredge has a solid steel handle etched with tiny runes, wrapped in blue dragon hide with a star sapphire set into the pommel. The axe head is forged from silver, electrum, and steel alloys whose edges constantly shimmer with a deep blue luminescence.\n   You gain a +3 bonus to attack and damage rolls made with this magic weapon. The Shield spell provides no defense against the axe, which passes through that spell's barrier of magical force.\n   When you hit a fiend or an undead with the axe, cold blue flames erupt from its blade and deal an extra 2d6 radiant damage to the target.\n   >>Hurling<<. The battleaxe has 3 charges. You can expend 1 charge and make a ranged attack with the axe, hurling it as if it had the thrown property with a normal range of 60 feet and a long range of 180 feet. Whether it hits or misses, the axe flies back to you at the end of the current turn, landing in your open hand or at your feet in your space (as you choose). The axe regains all expended charges daily at dawn.\n   >>Illumination<<. While holding the axe, you can use an action to cause the axe to glow blue or to quench the glow. This glow sheds bright light in a 30-foot radius and dim light for an additional 30 feet.\n   >>Sentience<<. Azuredge is a sentient lawful neutral weapon with an Intelligence of 12, a Wisdom of 15, and a Charisma of 15. It has hearing and darkvision out to a range of 120 feet.\n   The weapon communicates telepathically with its wielder and can speak, read, and understand Common. It has a calm, delicate voice. The weapon can sense the presence of non-lawful creatures within 120 feet of it.\n   >>Personality<<. Azuredge is sworn to protect Waterdeep, and it desires to be wielded by a law-abiding person willing to dedicate everything to the city's defense. The weapon is patient and takes its time finding its ideal wielder.\n   If someone tries to use Azuredge against its will, the axe can become ten times heavier than normal, and can magically adhere to any Medium or larger object or surface it comes into contact with. Once it does so, the axe can't be wielded. Nothing short of a Wish spell can separate the axe from the item or surface to which it is adhered without destroying one or the other, though the axe can choose to end the effect at any time.";
 MagicItemsList["azuredge"] = {
