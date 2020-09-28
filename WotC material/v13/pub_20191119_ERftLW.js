340de41a1736726c801e5aa356c7d6d064118f6b
Braab Himself <63005585+BraabHimself@users.noreply.github.com>
2020-09-28 05:08:15 -0400

    Correct fastieth trait (#65)
    fastieth was mispelled as 'fastiets'

--- WotC material/v13/pub_20191119_ERftLW.js
+++ WotC material/v13/pub_20191119_ERftLW.js
@@ -3710,6 +3710,6 @@ CreatureList["fastieth"] = {
 	}],
 	traits : [{
 		name : "Quickness (Recharge 5-6)",
-		description : "The fastiets can take the Dodge action as a bonus action."
+		description : "The fastieth can take the Dodge action as a bonus action."
 	}]
 };
